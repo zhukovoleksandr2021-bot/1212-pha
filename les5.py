b9eba893ec08210dd3c1453a6ca619b61ba712d5
Worstation <zhukovoleksandr2021@gmail.com>
2026-02-19 16:09:17 +0200

    less4

--- les5.py
+++ les5.py
@@ -1,12 +1,21 @@
 class Parent:
     def __init__(self):
         self.height = 180
+        self.weight = 90
+
+    def intro(self):
+        print(f"My height is {self.height}")
+
+    def hz(self):
+        print(f"My weight is {self.weight}")
 
 class Child(Parent):
     def __init__(self):
         super().__init__()
         self.height += 5
-        print(self.height)
+        self.intro()
+        self.weight -= 15
+        self.hz()
 
 johan = Parent()
 mark_5 = Child()
