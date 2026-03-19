6c4dbe2a0792620474229c12df1f6942bfd90f31
student <zhukovoleksandr2021@gmail.com>
2026-03-19 15:30:23 +0200

    les7

--- les6.1.py
+++
@@ -1,8 +0,0 @@
-path = input("path: ")
-
-try:
-    with open(path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-except FileNotFoundError as fnfe:
-    print(f"File {path} not")
